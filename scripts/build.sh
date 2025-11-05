1c7dce0c61d2afa902f749084a582d8c7926ae8f
Tobias Locker <tobias@tobiaslocker.de>
2025-11-05 20:15:01 +0100

    Move build script

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pandoc cv.md \
-            --template=template.tex \
-            --metadata-file=metadata.yaml \
-            --pdf-engine=xelatex \
-            -o build/pdf/cv.pdf
